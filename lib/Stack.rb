c061a5eeb65e2b86628a48a8ac5dba26ef1fc916
Ann Dai <gofarann@gmail.com>
2017-02-23 12:09:54 -0800

    added methods to stacks class, passed test specs

--- lib/Stack.rb
+++ lib/Stack.rb
@@ -1,21 +1,33 @@
 class Stack
+  attr_reader :store
+
   def initialize
     @store = Array.new
   end
 
   def push(element)
+    @store << element
   end
 
   def pop
+    @store.pop
   end
 
   def top
+    @store[-1]
   end
 
   def size
+    @store.size
   end
 
   def empty?
+    return size == 0
+    # if @store.size == 0
+    #   return true
+    # else
+    #   return false
+    # end
   end
 
   def to_s
