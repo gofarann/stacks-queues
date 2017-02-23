691b7c57add60ebe9b77a020f4ebdd09777f7539
Ann Dai <gofarann@gmail.com>
2017-02-23 12:45:35 -0800

    queue class created with methods, spec tests passed

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,29 @@
 class Queue
   def initialize
+    @store = Array.new
   end
 
   def enqueue(element)
+    @store << element
   end
 
   def dequeue
+    @store.shift
   end
 
   def front
+    return @store[0]
   end
 
   def size
+    @store.size
   end
 
   def empty?
+    return size == 0
   end
 
   def to_s
+    return @store.to_s
   end
 end
